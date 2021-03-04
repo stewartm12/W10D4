fe7f8e6800ec404753cb66e5a5459b16871ec01c
stewart morales <stewartmorales129@gmail.com>
2021-03-04 15:43:28 -0500

    fixed bug in todosReducer

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,3 +1,28 @@
+import {RECEIVE_TODOS, RECEIVE_TODO} from '../actions/todo_actions';
+
+
+const todosReducer = (state = initialState, action) => { // action comes from dispatch
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+    debugger
+    switch (action.type) {
+        case RECEIVE_TODOS: 
+            let futureState = {};
+            for (let key in action.todos) {
+                key = parseInt(key);
+                futureState[key+1] = action.todos[key];
+            }
+            return futureState;
+        case RECEIVE_TODO: 
+            nextState[action.todo.id] = action.todo;
+            return nextState;
+        default:
+            return state;
+    }
+};
+
+export default todosReducer;
+
 const initialState = {
     1: {
         id: 1,
@@ -12,12 +37,3 @@ const initialState = {
         done: true
     }
 };
-
-const todosReducer = (state = initialState, action) => { // action comes from dispatch
-    switch (action.type) {
-        default:
-            return state;
-    }
-};
-
-export default todosReducer;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,10 +1,12 @@
 import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
 
   const store = configureStore();
   window.store = store;
-
+  window.receiveTodos = receiveTodos;
+  window.receiveTodo = receiveTodo;
   // root.innerHTML = '<h1>Todos App</h1>';
 })
