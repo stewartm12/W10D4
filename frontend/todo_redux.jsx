3f5110437635312f881c761e361ab3289eda26b2
stewart morales <stewartmorales129@gmail.com>
2021-03-05 13:57:11 -0500

    Create Root and App components

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,14 +1,12 @@
 import configureStore from './store/store';
-import { receiveTodos, receiveTodo, removeTodo } from './actions/todo_actions';
-import { receiveSteps, receiveStep, removeStep } from './actions/step_actions';
+// import { receiveTodos, receiveTodo, removeTodo } from './actions/todo_actions';
+// import { receiveSteps, receiveStep, removeStep } from './actions/step_actions';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.store = store;
-  window.receiveTodos = receiveTodos;
-  window.receiveTodo = receiveTodo;
-  window.removeTodo = removeTodo;
-  window.receiveSteps = receiveSteps;
-  window.receiveStep = receiveStep;
-  window.removeStep = removeStep;
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<Root store={store}/>, rootElement);
 })
